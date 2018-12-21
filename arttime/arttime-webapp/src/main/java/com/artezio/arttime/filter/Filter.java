cb2853443e2b18705dbcd0d458e404fe1ff47dbe
Eugene Shestakov <Evgeny.Shestakov@artezio.com>
2018-12-21 15:54:19 +0300

    Filters are now saved using AJAX, empty filter name won't cause exception

--- arttime/arttime-webapp/src/main/java/com/artezio/arttime/filter/Filter.java
+++ arttime/arttime-webapp/src/main/java/com/artezio/arttime/filter/Filter.java
@@ -14,6 +14,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import javax.persistence.*;
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import com.artezio.javax.jpa.abac.AbacRule;
@@ -40,6 +41,7 @@ public class Filter implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String name;
     private String owner;
     @ManyToMany
