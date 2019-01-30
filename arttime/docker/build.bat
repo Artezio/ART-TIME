f8dd7c318b1b51c6a088d2a481f9fe86f2081962
Eugene Shestakov <Evgeny.Shestakov@artezio.com>
2019-01-30 11:36:58 +0300

    Enabled internal network. Added arttime db import sql

--- arttime/docker/build.bat
+++ arttime/docker/build.bat
@@ -2,5 +2,4 @@ docker build --tag=arttime-demo/webapp ./webapp/
 docker build --tag=arttime-demo/keycloak ./keycloak/
 docker build --tag=arttime-demo/database ./database
 
-docker network create --internal arttime-internal-network
-docker-compose up
+docker-compose up -d
