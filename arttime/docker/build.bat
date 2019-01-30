d155a854be365131f9615e30875f98dde256375e
aberezovski <Artem.Berezovski@artezio.com>
2019-01-30 10:18:42 +0300

    ATIME-1334

--- arttime/docker/build.bat
+++ arttime/docker/build.bat
@@ -1,8 +1,6 @@
-cd webapp
-docker build --tag=arttime-demo/webapp .
-cd ..
-cd database
-docker build --tag=arttime-demo/database .
-cd ..
+docker build --tag=arttime-demo/webapp ./webapp/
+docker build --tag=arttime-demo/keycloak ./keycloak/
+docker build --tag=arttime-demo/database ./database
+
 docker network create --internal arttime-internal-network
 docker-compose up
