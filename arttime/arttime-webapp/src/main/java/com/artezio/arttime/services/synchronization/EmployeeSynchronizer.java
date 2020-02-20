32362a126f80d44a009b8d017c8fe9b8fd1cc5fe
Nikolay Matokh <nikolay.matokh@artezio.com>
2020-02-20 17:51:16 +0300

    ATIME-1404: run employee/team synchronization in separate transactions

--- arttime/arttime-webapp/src/main/java/com/artezio/arttime/services/synchronization/EmployeeSynchronizer.java
+++ arttime/arttime-webapp/src/main/java/com/artezio/arttime/services/synchronization/EmployeeSynchronizer.java
@@ -1,39 +1,74 @@
 package com.artezio.arttime.services.synchronization;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.inject.Inject;
+import javax.transaction.UserTransaction;
+
 import com.artezio.arttime.datamodel.Employee;
 import com.artezio.arttime.services.EmployeeService;
 import com.artezio.arttime.services.integration.EmployeeTrackingSystem;
 
-import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.inject.Named;
-import java.util.List;
-
-@Named
 @Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
 public class EmployeeSynchronizer implements EmployeeSynchronizerLocal {
+
+    private static final String SYNCHRONIZATION_ERROR_MSG = "User {0} synchronization error";
+    private static final String SYNCHRONIZATION_ERROR_DETAILS_MSG = "User synchronization error details";
+
+    private Logger log = Logger.getLogger(EmployeeSynchronizer.class.getName());
 
     @Inject
     private EmployeeTrackingSystem employeeTrackingSystem;
     @Inject
     private EmployeeService employeeService;
+    @Resource
+    private UserTransaction transaction;
 
     public void synchronizeEmployees() {
-        List<Employee> employees = employeeService.getAll();
-        for (Employee employee : employees) {
+        employeeService.getAll().forEach(this::synchronize);
+    }
+
+    private void synchronize(Employee employee) {
+        try {
             String userName = employee.getUserName();
             Employee externalEmployee = employeeTrackingSystem.findEmployee(userName);
-            if (externalEmployee != null) {
-                employee.setFirstName(externalEmployee.getFirstName());
-                employee.setLastName(externalEmployee.getLastName());
-                employee.setEmail(externalEmployee.getEmail());
-                employee.setDepartment(externalEmployee.getDepartment());
-                employee.setFormer(false);
-            } else {
-                employee.setFormer(true);
-            }
-            employeeService.update(employee);
+            update(employee, externalEmployee);
+        } catch (Exception e) {
+            log.log(Level.WARNING, SYNCHRONIZATION_ERROR_MSG, employee.getUserName());
+            log.log(Level.FINE, SYNCHRONIZATION_ERROR_DETAILS_MSG, e);
         }
     }
+
+    private void update(Employee target, Employee source) throws Exception {
+        if (source != null) {
+            target.setFirstName(source.getFirstName());
+            target.setLastName(source.getLastName());
+            target.setEmail(source.getEmail());
+            target.setDepartment(source.getDepartment());
+            target.setFormer(false);
+        } else {
+            target.setFormer(true);
+        }
+        
+        save(target);
+    }
+
+    private void save(Employee employee) throws Exception {
+        try {
+            transaction.begin();
+            employeeService.update(employee);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        }
+    }
+    
 
 }
